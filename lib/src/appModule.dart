547a5ef9b095efe51d4a0abf9ea9f490c9845188
Matheus Pivato <matheuspivato@outlook.com>
2023-02-24 23:16:35 -0300

    um pouco de estrutura ao backend

--- lib/src/appModule.dart
+++ lib/src/appModule.dart
@@ -1,8 +1,18 @@
+import 'package:PraticFitBackend/src/core/services/database/remoteDatabase.dart';
+import 'package:PraticFitBackend/src/core/services/dotEnv/dotEnvService.dart';
 import 'package:PraticFitBackend/src/modules/user/userResouce.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
+import 'core/services/database/postgres/postgresDatabase.dart';
 
 class AppModule extends Module {
+  @override
+  List<Bind> get binds => [
+        Bind.instance<DotEnvService>(DotEnvService.instance),
+        Bind.singleton<RemoteDatabase>((i) => PostgresDatabase(i())),
+      ];
+
+  @override
   List<ModularRoute> get routes => [
         Route.get('/', (Request request) => Response.ok('Home')),
         Route.resource(UserResource())
