f4b9d68c39e6687ce5ee5ee96ef40f6965090635
Matheus Pivato <matheuspivato@outlook.com>
2023-02-27 14:23:58 -0300

    crud user completo

--- lib/src/modules/user/userResouce.dart
+++ lib/src/modules/user/userResouce.dart
@@ -11,14 +11,13 @@ class UserResource extends Resource {
         Route.get('/user/:id', _getUserById),
         Route.post('/user', _createUser),
         Route.put('/user/:id', _updateUser),
-        Route.post('/userUpdate/:id', _userUpdate),
         Route.delete('/user/:id', _deleteUser),
       ];
 
   FutureOr<Response> _getAllUser(Injector injector) async {
     final database = injector.get<RemoteDatabase>();
     final result = await database
-        .query('SELECT id, name, email, role FROM public."User";');
+        .query('SELECT id, nome, email, role FROM public."User";');
     final listUser = result.map((e) => e['User']).toList();
     return Response.ok(jsonEncode(listUser));
   }
@@ -28,7 +27,7 @@ class UserResource extends Resource {
     final id = arguments.params['id'];
     final database = injector.get<RemoteDatabase>();
     final result = await database.query(
-        'SELECT id, name, email, role FROM public."User" WHERE id = @id;',
+        'SELECT id, nome, email, role FROM public."User" WHERE id = @id;',
         variables: {'id': id});
     final userMap = result.map((element) => element['User']).first;
     return Response.ok(jsonEncode(userMap));
@@ -36,36 +35,40 @@ class UserResource extends Resource {
 
   FutureOr<Response> _createUser(
       ModularArguments arguments, Injector injector) async {
-    final userMap = arguments.data as Map;
-
-    userMap.remove('id');
-
+    final userParams = (arguments.data as Map).cast<String, dynamic>();
+    userParams.remove('id');
     final database = injector.get<RemoteDatabase>();
-    final user = arguments.data;
     final result = await database.query(
-        'INSERT INTO public."User" (name, email, password, role) VALUES (@name, @email, @password, @role) RETURNING id;',
-        variables: {
-          'name': user['name'],
-          'email': user['email'],
-          'password': user['password'],
-          'role': user['role']
-        });
-    final id = result.map((e) => e['User']).first!['id'];
-    return Response.ok(jsonEncode({'id': id}));
+        'INSERT INTO "User" (nome, email, senha) VALUES (@nome, @email, @senha) RETURNING id, email, nome;',
+        variables: userParams);
+    final userMap = result.map((e) => e['User']).first;
+    return Response.ok(jsonEncode(userMap));
   }
 
   FutureOr<Response> _updateUser(
       ModularArguments arguments, Injector injector) async {
-    return Response.ok('Updated user: ${arguments.data}');
-  }
+    final userParams = (arguments.data as Map).cast<String, dynamic>();
 
-  FutureOr<Response> _userUpdate(
-      ModularArguments arguments, Injector injector) async {
-    return Response.ok('Updated user: ${arguments.params['id']}');
+    final columns = userParams.keys
+        .where((element) => element != 'id' || element != 'senha')
+        .map(
+          (key) => '$key=@$key',
+        )
+        .toList();
+    final database = injector.get<RemoteDatabase>();
+    final result = await database.query(
+        'UPDATE "User" SET ${columns.join(',')} WHERE id = @id RETURNING id, email, nome;',
+        variables: userParams);
+    final userMap = result.map((e) => e['User']).first;
+    return Response.ok(jsonEncode(userMap));
   }
 
   FutureOr<Response> _deleteUser(
       ModularArguments arguments, Injector injector) async {
-    return Response.ok('Deleted user: ${arguments.params['id']}');
+    final id = arguments.params['id'];
+    final database = injector.get<RemoteDatabase>();
+    await database
+        .query('DELETE FROM "User" WHERE id = @id;', variables: {'id': id});
+    return Response.ok('Deleted user: $id');
   }
 }
