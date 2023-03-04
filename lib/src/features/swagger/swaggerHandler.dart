79dcf29127040916ff21b55580a907fb34e271db
Matheus Pivato <matheuspivato@outlook.com>
2023-03-03 23:03:50 -0300

    bloqueando acesso a documentação quando em produção

--- lib/src/features/swagger/swaggerHandler.dart
+++ lib/src/features/swagger/swaggerHandler.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
+import 'package:PraticFitBackend/src/core/constants/isProduction.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_swagger_ui/shelf_swagger_ui.dart';
 
 // Método responsável por gerenciar as rotas do Swagger
 FutureOr<Response> swaggerHandler(Request request) {
-  final path = 'specs/swagger.yaml';
+  final path = Production ? '' : 'specs/swagger.yaml';
   final handler = SwaggerUI(
     path,
-    title: 'Pratic Fit',
+    title: 'Pratic Fit Documentation',
     deepLink: true,
   );
   return handler(request);
---
+++ lib/src/features/user/userModule.dart
@@ -0,0 +1,9 @@
+import 'package:shelf_modular/shelf_modular.dart';
+
+class UserModule extends Module {
+  @override
+  List<Bind<Object>> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [];
+}
